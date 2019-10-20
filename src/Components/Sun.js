737c4fc35f330178d818c2e7556830b3f82281a4
Nils <nilsnathorstwindahl@gmail.com>
2019-10-20 18:11:23 +0200

    refactored

--- src/Components/Sun.js
+++ src/Components/Sun.js
@@ -1,20 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useRender } from "react-three-fiber";
-
-const Sun = ({ position }) => {
+import * as THREE from "three";
+const Sun = () => {
   const sunRef = useRef();
-  useRender(() => {
-    sunRef.current.position.y += 0.05;
-    sunRef.current.position.setLength(13);
-  });
+  var angle = 0;
+  const grpRef = useRef();
+  const date = new Date();
+  const [timeOfDay, setTimeOfDay] = useState(0);
+  useEffect(() => {
+    setTimeOfDay(date.getHours());
+  }, []);
 
+  useEffect(() => {
+    console.log(sunRef);
+  }, [grpRef]);
+  useRender(() => {
+    sunRef.current.rotation.z += 0.03;
+  });
+  console.log(timeOfDay);
   return (
-    <group ref={sunRef} position={[...position]}>
-      <mesh visible>
-        <meshLambertMaterial attach="material" color="yellow" />
-        <sphereBufferGeometry attach="geometry" args={[1, 32, 32]} />
+    <group ref={sunRef} position={[0, 0, 0]}>
+      <mesh position={[10, 0, 0]} visible>
+        <meshLambertMaterial
+          attach="material"
+          color={timeOfDay > 17 ? "blue" : "yellow"}
+        />
+        <boxBufferGeometry attach="geometry" args={[2, 2, 2]} />
       </mesh>
-      <pointLight position={[...position]} intensity={2} castShadow={true} />
+      <pointLight position={[10, 0, 0]} intensity={2} castShadow={true} />
     </group>
   );
 };
