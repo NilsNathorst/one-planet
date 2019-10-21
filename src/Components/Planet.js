a44284d5b2d1d1e0582b5ba4ca553e6e88b73531
Adrian Jungnelius <a.jungnelius@gmail.com>
2019-10-21 13:23:50 +0200

    added context, onClick to get vector3, new model

--- src/Components/Planet.js
+++ src/Components/Planet.js
@@ -1,39 +1,78 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import * as CANNON from "cannon";
 import { useCannon } from "../helpers/useCannon";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useThree } from "react-three-fiber";
+import { CanvasContext } from "./Context";
 
 const Planet = ({ position }) => {
-  const [model, setModel] = useState(null);
   const surfaceRef = useRef();
+  const [model, setModel] = useState(null);
+  const [active, setActive] = useState(false);
+  const { setTreeVectors } = useContext(CanvasContext);
+  const workableSurfaceRef = useRef();
   const waterRef = useRef();
   const planetRef = useCannon({ mass: 0 }, body => {
     body.addShape(new CANNON.Sphere(4.9));
     body.position.set(...position);
   });
+
+  const {
+    intersect // Calls onMouseMove handlers for objects underneath the cursor
+  } = useThree();
+
   useEffect(() => {
-    new GLTFLoader().load("/models/planet/planet.gltf", setModel);
+    new GLTFLoader().load("/models/planet/newplanet.gltf", setModel);
   }, []);
 
   useEffect(() => {
-    model && planetRef.current.scale.set(4, 4, 4);
-    model && console.log(model.scene.children[0].geometry);
+    model && planetRef.current.scale.set(3.2, 3.2, 3.2);
+    model && console.log(model.scene);
+
+    model && workableSurfaceRef.current.rotateX(-Math.PI / 2);
+    model && workableSurfaceRef.current.scale.set(1.01, 1.01, 1.01);
   }, [model]);
 
   return (
     model && (
-      <group ref={planetRef} position={[0, 0, 0]} receiveShadow={true}>
-        <mesh ref={surfaceRef}>
+      <group ref={planetRef} position={[0, 0, 0]}>
+        <mesh ref={surfaceRef} receiveShadow>
+          <bufferGeometry
+            attach="geometry"
+            {...model.scene.children[1].geometry}
+          />
+          <meshLambertMaterial attach="material" color="brown" />
+        </mesh>
+        <mesh
+          ref={workableSurfaceRef}
+          receiveShadow
+          onPointerOver={() => {
+            setActive(true);
+          }}
+          onPointerOut={() => {
+            setActive(false);
+          }}
+          onClick={() => {
+            setTreeVectors(treeVectors => [
+              ...treeVectors,
+              intersect()[0].point
+            ]);
+          }}
+        >
           <bufferGeometry
             attach="geometry"
             {...model.scene.children[0].geometry}
           />
-          <meshLambertMaterial attach="material" color="#1F5C00" />
+          <meshLambertMaterial
+            visible={active ? true : false}
+            attach="material"
+            wireframe={active ? true : false}
+          />
         </mesh>
-        <mesh ref={waterRef}>
+        <mesh ref={waterRef} receiveShadow>
           <bufferGeometry
             attach="geometry"
-            {...model.scene.children[1].geometry}
+            {...model.scene.children[2].geometry}
           />
           <meshLambertMaterial attach="material" color="#158BC6" />
         </mesh>
