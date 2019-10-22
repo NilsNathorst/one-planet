419de1f290abe99af77fb7c3b7ea5b07465db7a0
Adrian Jungnelius <a.jungnelius@gmail.com>
2019-10-22 14:59:20 +0200

    added vector function and postion on mesh

--- src/Components/Tree.js
+++ src/Components/Tree.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useRender } from "react-three-fiber";
+import generateVector3Between from "../helpers/generateVector3Between";
 
 const Tree = ({ position, variant }) => {
   const [model, setModel] = useState(null);
@@ -24,9 +25,6 @@ const Tree = ({ position, variant }) => {
   useEffect(() => {
     if (model) {
       const rotationOffset = Math.random() * 3;
-      const size = Math.random() * 0.005 + 0.006;
-      trunkRef.current.scale.set(size, size, size);
-      leavesRef.current.scale.set(size, size, size);
       trunkRef.current.geometry.rotateX(-Math.PI / 2);
       leavesRef.current.geometry.rotateX(-Math.PI / 2);
       trunkRef.current.geometry.rotateZ(rotationOffset);
@@ -37,15 +35,15 @@ const Tree = ({ position, variant }) => {
 
     //   treeRef.current.geometry.rotateX(-Math.PI / 2);
   }, [model]);
-
+  const vectors = generateVector3Between(0.005, 0.01);
   return (
     model && (
       <group position={[...position]} ref={groupRef}>
-        <mesh ref={trunkRef} castShadow>
+        <mesh ref={trunkRef} castShadow scale={vectors}>
           <bufferGeometry attach="geometry" {...model.children[1].geometry} />
           <meshStandardMaterial attach="material" color="brown" />
         </mesh>
-        <mesh ref={leavesRef} castShadow>
+        <mesh ref={leavesRef} castShadow scale={vectors}>
           <bufferGeometry attach="geometry" {...model.children[0].geometry} />
           <meshStandardMaterial attach="material" color="green" />
         </mesh>
