91a3b79e34be14bd8957e1b8ae30c09750b75a76
Nils <nilsnathorstwindahl@gmail.com>
2019-10-26 23:06:46 +0200

    refactored transparency, no longer flickers

--- src/Components/GraphicalComponents/Ocean.js
+++ src/Components/GraphicalComponents/Ocean.js
@@ -1,30 +1,37 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "react-three-fiber";
-import React, { useRef, useMemo } from "react";
-
+import React, { useRef, useMemo, useEffect, useState } from "react";
+import { useSpring, a, config } from "react-spring/three";
 import * as THREE from "three";
 const Ocean = () => {
   const gltf = useLoader(GLTFLoader, "/models/planet/planet-v4.gltf");
   const ref = useRef();
-
-  const texture = useMemo(() =>
-    new THREE.TextureLoader().load("/assets/watermap.png")
-  );
-
+  const [rev, setRev] = useState(false);
+  const { scale, ...props } = useSpring({
+    scale: [29.5, 29.5, 29.5],
+    from: { scale: [28.5, 28.5, 28.5] },
+    reset: true,
+    onRest: () => {
+      setRev(!rev);
+    },
+    reverse: rev,
+    config: { ...config.molasses, duration: 1000 * 30 }
+  });
+  useEffect(() => {
+    console.log(ref);
+  }, []);
   return (
     <>
-      <mesh
-        comuteVertexNormals
-        ref={ref}
-        name="Ocean"
-        scale={[29, 29, 29]}
-        position={[0, 0, 0]}
-      >
+      <a.mesh name="Ocean" scale={scale} position={[0, 0, 0]}>
         <bufferGeometry attach="geometry" {...gltf.__$[3].geometry} />
-        <meshStandardMaterial attach="material" color="dodgerblue" transparent>
-          <primitive attach="map" object={texture} />
-        </meshStandardMaterial>
-      </mesh>
+        <meshStandardMaterial
+          attach="material"
+          ref={ref}
+          blending={THREE.CustomBlending}
+          blendDst={THREE.SrcColorFactor}
+          color={0x2191fb}
+        ></meshStandardMaterial>
+      </a.mesh>
     </>
   );
 };
