a79bfb0e6c336bb85cd58273959a3d4e371ff9c9
Nils <nilsnathorstwindahl@gmail.com>
2019-10-31 13:56:33 +0100

    added cloud rotation

--- src/Components/GraphicalComponents/Clouds.js
+++ src/Components/GraphicalComponents/Clouds.js
@@ -6,23 +6,32 @@ import { a } from "react-spring/three";
 import * as THREE from "three";
 
 const Cloud = ({ pos }) => {
-  const gltf = useLoader(GLTFLoader, "/models/cloud/newcloud.gltf");
+  const gltf = useLoader(GLTFLoader, "/models/cloud/cloudcube.gltf");
   const ref = useRef();
-
+  const grpRef = useRef();
+  console.log(gltf);
+  let xR = Math.random() * 0.0003;
+  let yR = Math.random() * 0.0003;
+  let zR = Math.random() * 0.0003;
   useEffect(() => {
     ref.current.lookAt(0, 0, 0);
-    console.log(ref.current);
   }, []);
-
+  useFrame(() => {
+    grpRef.current.rotation.x += xR;
+    grpRef.current.rotation.z += zR;
+    grpRef.current.rotation.y += yR;
+  });
   return (
-    <a.mesh ref={ref} castShadow position={pos} scale={[0.2, 0.2, 0.2]}>
-      <bufferGeometry attach="geometry" {...gltf.__$[1].geometry} />
-      <meshNormalMaterial
-        attach="material"
-        // opacity={0.85}
-        transparent
-      ></meshNormalMaterial>
-    </a.mesh>
+    <a.group ref={grpRef}>
+      <a.mesh position={pos} ref={ref} castShadow scale={[0.2, 0.2, 0.2]}>
+        <bufferGeometry attach="geometry" {...gltf.__$[1].geometry} />
+        <meshBasicMaterial
+          attach="material"
+          opacity={0.85}
+          transparent
+        ></meshBasicMaterial>
+      </a.mesh>
+    </a.group>
   );
 };
 const Clouds = () => {
