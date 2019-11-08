2cd2eebe85744c13c7a23778bd69c1dcc10a25f7
Nils <nilsnathorstwindahl@gmail.com>
2019-11-08 14:08:19 +0100

    added outlinePass

--- src/Components/PostProcessing/Fx.js
+++ src/Components/PostProcessing/Fx.js
@@ -1,35 +1,54 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, Suspense } from "react";
 import { extend, useFrame, useThree } from "react-three-fiber";
+import { connect } from "react-redux";
+
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { BloomPass } from "three/examples/jsm/postprocessing/BloomPass";
-import { FilmPass } from "three/examples/jsm/postprocessing/FilmPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass";
+import * as THREE from "three";
+extend({ EffectComposer, RenderPass, UnrealBloomPass, OutlinePass });
 
-extend({ EffectComposer, RenderPass, FilmPass, BloomPass });
-
-function Effects() {
+function Fx({ showInfo, name }) {
   const { gl, scene, camera, size } = useThree();
-  const filmp = useRef();
-  const bloomp = useRef();
+
+  const outlineP = useRef();
   const composer = useRef();
-  useEffect(() => {
-    console.log(filmp, bloomp);
-  }, []);
+  const res = new THREE.Vector2(1024, 1024);
   useEffect(() => void composer.current.setSize(size.width, size.height), [
     size
   ]);
-  useFrame(() => composer.current.render(), 1);
 
+  useFrame(() => composer.current.render(), 1);
+  useEffect(() => {}, [showInfo]);
   return (
-    <effectComposer ref={composer} args={[gl]}>
-      <renderPass attachArray="passes" args={[scene, camera]} />
-      {/* <filmPass
-        ref={filmp}
-        attachArray="passes"
-        args={[0.35, 0.025, 648, false]}
-        renderToScreen
-      /> */}
-    </effectComposer>
+    <Suspense fallback={null}>
+      <effectComposer ref={composer} args={[gl]}>
+        <renderPass attachArray="passes" args={[scene, camera]} />
+        {/* <unrealBloomPass attachArray="passes" /> */}
+        {name === "QUERY" && (
+          <outlinePass
+            ref={outlineP}
+            attachArray="passes"
+            args={[
+              res,
+              scene,
+              camera,
+              showInfo.active ? [showInfo.object] : []
+            ]}
+            pulsePeriod={4}
+            renderToScreen
+          />
+        )}
+      </effectComposer>
+    </Suspense>
   );
 }
-export default Effects;
+const mapStateToProps = ({ state: { showInfo, name } }) => {
+  return {
+    showInfo,
+    name
+  };
+};
+
+export default connect(mapStateToProps)(Fx);
