c8843784b057dfd351f2825c8eda74608e79d9eb
Nils <nilsnathorstwindahl@gmail.com>
2019-11-13 14:59:41 +0100

    updated to new model

--- src/Components/GraphicalComponents/Dirt.js
+++ src/Components/GraphicalComponents/Dirt.js
@@ -1,15 +1,114 @@
-import React from "react";
+import React, { useEffect, Suspense } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { useLoader } from "react-three-fiber";
-
+import * as THREE from "three";
 const Dirt = () => {
-  const gltf = useLoader(GLTFLoader, "/models/planet/continentsplanet.gltf");
+  const gltf = useLoader(GLTFLoader, "/models/planet/new.glb");
+  const [colorMap, bumpMap, normalMap] = useLoader(TextureLoader, [
+    "/assets/textures/Terrain/Vol_19_4_Base_Color.png",
+    "/assets/textures/Terrain/Vol_19_4_Height.png",
+    "/assets/textures/Terrain/Vol_19_4_Normal.png"
+  ]);
 
   return (
-    <mesh receiveShadow scale={[30.3, 30.3, 30.3]} position={[0, 0, 0]}>
-      <bufferGeometry attach="geometry" {...gltf.__$[1].geometry} />
-      <meshStandardMaterial attach="material" color="sienna" roughness={1} />
-    </mesh>
+    <Suspense fallback={null}>
+      <group>
+        <mesh
+          receiveShadow
+          scale={[12, 12, 12]}
+          position={[0, 0, 0]}
+          onPointerOver={e => e.stopPropagation()}
+        >
+          <bufferGeometry attach="geometry" {...gltf.__$[5].geometry} />
+          <meshStandardMaterial attach="material" roughness={1}>
+            <primitive
+              attach="map"
+              object={colorMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="bumpMap"
+              object={bumpMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="normalMap"
+              object={normalMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+          </meshStandardMaterial>
+        </mesh>
+        <mesh
+          receiveShadow
+          scale={[12, 12, 12]}
+          position={[0, 0, 0]}
+          onPointerOver={e => e.stopPropagation()}
+        >
+          <bufferGeometry attach="geometry" {...gltf.__$[6].geometry} />
+          <meshStandardMaterial attach="material" roughness={1}>
+            <primitive
+              attach="map"
+              object={colorMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="bumpMap"
+              object={bumpMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="normalMap"
+              object={normalMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+          </meshStandardMaterial>
+        </mesh>
+        <mesh
+          receiveShadow
+          scale={[12, 12, 12]}
+          position={[0, 0, 0]}
+          onPointerOver={e => e.stopPropagation()}
+        >
+          <bufferGeometry attach="geometry" {...gltf.__$[7].geometry} />
+          <meshStandardMaterial attach="material" roughness={1}>
+            <primitive
+              attach="map"
+              object={colorMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="bumpMap"
+              object={bumpMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+            <primitive
+              attach="normalMap"
+              object={normalMap}
+              wrapT={THREE.RepeatWrapping}
+              wrapS={THREE.RepeatWrapping}
+              repeat={[4, 4]}
+            />
+          </meshStandardMaterial>
+        </mesh>
+      </group>
+    </Suspense>
   );
 };
 
--- src/Components/GraphicalComponents/Surface.js
+++ src/Components/GraphicalComponents/Surface.js
@@ -1,9 +1,10 @@
 import React, { useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { useLoader } from "react-three-fiber";
 import { connect } from "react-redux";
 import { addTree, setShowInfo, setPlantable } from "../../actions";
-
+import * as THREE from "three";
 const Surface = ({
   plantable,
   addTree,
@@ -13,7 +14,13 @@ const Surface = ({
   isDead
 }) => {
   const ref = useRef();
-  const gltf = useLoader(GLTFLoader, "/models/planet/continentsplanet.gltf");
+  const gltf = useLoader(GLTFLoader, "/models/planet/new.glb");
+  const [colorMap, bumpMap, normalMap, aoMap] = useLoader(TextureLoader, [
+    "/assets/textures/Grass/Vol_42_1_Base_Color.png",
+    "/assets/textures/Grass/Vol_42_1_Height.png",
+    "/assets/textures/Grass/Vol_42_1_Normal.png",
+    "/assets/textures/Grass/Vol_42_1_Ambient_Occlusion.png"
+  ]);
   const hover = e => {
     e.stopPropagation();
     name === "QUERY" &&
@@ -40,76 +47,47 @@ const Surface = ({
   };
 
   return (
-    <group>
-      <mesh
-        onPointerOver={e => hover(e)}
-        onPointerOut={e => unhover(e)}
-        onPointerDown={e => handleClick(e)}
-        ref={ref}
-        receiveShadow
-        scale={[30.4, 30.4, 30.4]}
-        position={[0, 0, 0]}
-        name="africa"
-      >
-        <bufferGeometry attach="geometry" {...gltf.__$[3].geometry} />
-        <meshStandardMaterial
-          attach="material"
-          color={isDead ? "gray" : "green"}
-          roughness={1}
-        ></meshStandardMaterial>
-      </mesh>
-      <mesh
-        onPointerOver={e => hover(e)}
-        onPointerOut={e => unhover(e)}
-        onPointerDown={e => handleClick(e)}
-        ref={ref}
-        receiveShadow
-        scale={[30.4, 30.4, 30.4]}
-        position={[0, 0, 0]}
-        name="scandinavia"
-      >
-        <bufferGeometry attach="geometry" {...gltf.__$[6].geometry} />
-        <meshStandardMaterial
-          attach="material"
-          color={isDead ? "gray" : "#FFC857"}
-          roughness={1}
+    <mesh
+      onPointerOver={e => hover(e)}
+      onPointerOut={e => unhover(e)}
+      onPointerDown={e => handleClick(e)}
+      ref={ref}
+      receiveShadow
+      scale={[12.14, 12.14, 12.14]}
+      position={[0, 0, 0]}
+    >
+      <bufferGeometry attach="geometry" {...gltf.__$[8].geometry} />
+      <meshStandardMaterial attach="material" roughness={1}>
+        <primitive
+          attach="map"
+          object={colorMap}
+          wrapT={THREE.RepeatWrapping}
+          wrapS={THREE.RepeatWrapping}
+          repeat={[10, 10]}
         />
-      </mesh>
-      <mesh
-        onPointerOver={e => hover(e)}
-        onPointerOut={e => unhover(e)}
-        onPointerDown={e => handleClick(e)}
-        ref={ref}
-        receiveShadow
-        scale={[30.4, 30.4, 30.4]}
-        position={[0, 0, 0]}
-        name="oceania"
-      >
-        <bufferGeometry attach="geometry" {...gltf.__$[4].geometry} />
-        <meshStandardMaterial
-          attach="material"
-          color={isDead ? "gray" : "whitesmoke"}
-          roughness={1}
+        <primitive
+          attach="bumpMap"
+          object={bumpMap}
+          wrapT={THREE.RepeatWrapping}
+          wrapS={THREE.RepeatWrapping}
+          repeat={[10, 10]}
         />
-      </mesh>
-      <mesh
-        onPointerOver={e => hover(e)}
-        onPointerOut={e => unhover(e)}
-        onPointerDown={e => handleClick(e)}
-        ref={ref}
-        receiveShadow
-        scale={[30.4, 30.4, 30.4]}
-        position={[0, 0, 0]}
-        name="america"
-      >
-        <bufferGeometry attach="geometry" {...gltf.__$[5].geometry} />
-        <meshStandardMaterial
-          attach="material"
-          color={isDead ? "gray" : "hotpink"}
-          roughness={1}
+        <primitive
+          attach="normalMap"
+          object={normalMap}
+          wrapT={THREE.RepeatWrapping}
+          wrapS={THREE.RepeatWrapping}
+          repeat={[10, 10]}
         />
-      </mesh>
-    </group>
+        <primitive
+          attach="lightMap"
+          object={aoMap}
+          wrapT={THREE.RepeatWrapping}
+          wrapS={THREE.RepeatWrapping}
+          repeat={[10, 10]}
+        />
+      </meshStandardMaterial>
+    </mesh>
   );
 };
 const mapStateToProps = ({ state: { name, plantable, isDead } }) => {
