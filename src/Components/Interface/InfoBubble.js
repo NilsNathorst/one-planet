4f07435aa14829dfc118825d325c299ea990428c
Nils <nilsnathorstwindahl@gmail.com>
2019-11-15 12:10:45 +0100

    removed hover on surface

--- src/Components/Interface/InfoBubble.js
+++ src/Components/Interface/InfoBubble.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { animated, config, useSpring } from "react-spring";
 import infoData from "../../database/infoData.json";
-import { ReactComponent as PlaceholderIcon } from "../../assets/icons/help.svg";
+
 import { ReactComponent as TrashIcon } from "../../assets/icons/info/drinks.svg";
 import { ReactComponent as TreeIcon } from "../../assets/icons/info/forest.svg";
-import { ReactComponent as ContinentIcon } from "../../assets/icons/info/ancient.svg";
+
 import styled from "styled-components";
 import { connect } from "react-redux";
 const Container = styled(animated.div)`
@@ -33,10 +33,7 @@ const Container = styled(animated.div)`
     max-width: 40px;
   }
 `;
-const StyledContinentIcon = styled(ContinentIcon)`
-  width: 100%;
-  height: 100%;
-`;
+
 const StyledTrashIcon = styled(TrashIcon)`
   width: 100%;
   height: 100%;
@@ -61,11 +58,10 @@ const InfoBubble = ({ object, infoActive }) => {
     <Container style={props} className="info">
       <div className="div">
         {prevObj.objType === "trash" && <StyledTrashIcon />}
-        {prevObj.objType === "continent" && <StyledContinentIcon />}
         {prevObj.objType === "tree" && <StyledTreeIcon />}
       </div>
       <animated.p>
-        {prevObj.objType !== "tree" &&
+        {prevObj.objType === "trash" &&
           (infoData[prevObj.name] ? infoData[prevObj.name].message : null)}
         {prevObj.objType === "tree" &&
           (infoData[prevObj.name]
