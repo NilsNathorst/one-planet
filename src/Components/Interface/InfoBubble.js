367e9aa97749bdd62489179aca4b648d7388c8e1
Nils <nilsnathorstwindahl@gmail.com>
2019-11-16 12:54:39 +0100

    responsive fix

--- src/Components/Interface/InfoBubble.js
+++ src/Components/Interface/InfoBubble.js
@@ -8,6 +8,14 @@ import { ReactComponent as TreeIcon } from "../../assets/icons/info/forest.svg";
 import styled from "styled-components";
 import { connect } from "react-redux";
 const Container = styled(animated.div)`
+  @media screen and (min-width: 2500px) {
+    font-size: 2rem;
+  }
+  @media screen and (min-width: 1940px) {
+    font-size: 1.5rem;
+  }
+  font-size: 1rem;
+
   display: flex;
   transform: translateX(-50%);
   left: 50%;
@@ -18,19 +26,18 @@ const Container = styled(animated.div)`
   background: white;
   border-radius: 8px;
   color: black;
-  width: 300px;
+  min-width: 300px;
+  width: 20%;
   color: #888888;
   fill: #888888;
-  padding: 10px;
+  padding: 1em;
   p {
-    margin: auto;
     white-space: pre-line;
-    padding-left: 10px;
+    padding-left: 1em;
   }
   div {
-    margin: auto;
-    min-width: 40px;
-    max-width: 40px;
+    min-width: 20%;
+    max-width: 20%;
   }
 `;
 
@@ -44,7 +51,7 @@ const StyledTreeIcon = styled(TreeIcon)`
 `;
 const InfoBubble = ({ object, infoActive }) => {
   const props = useSpring({
-    marginBottom: infoActive ? 50 : -50,
+    marginBottom: infoActive ? 60 : -60,
     opacity: infoActive ? 1 : 0,
     config: config.stiff
   });
@@ -65,7 +72,9 @@ const InfoBubble = ({ object, infoActive }) => {
           (infoData[prevObj.name] ? infoData[prevObj.name].message : null)}
         {prevObj.objType === "tree" &&
           (infoData[prevObj.name]
-            ? `I'm a ${prevObj.age} ${infoData[prevObj.name].message}`
+            ? `I'm a ${prevObj.age} ${
+                infoData[prevObj.name].message
+              }. Without trees, the planet cannot survive. \n\nSelect the tree tool to plant more trees!`
             : null)}
       </animated.p>
     </Container>
